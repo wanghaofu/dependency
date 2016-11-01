830651fdec8f55b49a6a9e2a90005a267c071d2e
wangtao <wanghaofu@163.com>
2016-11-01 17:09:42 +0800

    up add getConfig

--- src/Dependency.php
+++ src/Dependency.php
@@ -15,6 +15,7 @@ class Dependency
     }
 
     /**
+     * 单例 工厂作用
      * @return static
      */
     public static function instance()
@@ -27,6 +28,9 @@ class Dependency
         return self::$instance[$class];
     }
 
+    /**
+     *  容器 ioc
+     */
     protected static function initContainer()
     {
         if (is_null(self::$container)) {
@@ -42,21 +46,8 @@ class Dependency
         return self::$container;
     }
 
-    protected function fetch($key)
-    {
-        return self::$container[$key];
-    }
-
-    public static function import(array $dependencies)
-    {
-        self::initContainer();
-
-        foreach ($dependencies as $key => $value) {
-            self::$container[static::prefix($key)] = $value;
-        }
-    }
-
     /**
+     * @desc 获取
      * @return mixed
      * @description  resource
      */
@@ -65,9 +56,49 @@ class Dependency
         return $this->packageFetch(static::CONFIG)->get($key);
     }
 
+
+    /**
+     * @desc 获取 same as packageConfig
+     * @return mixed
+     * @description  resource
+     */
+    public function getConfig($key)
+    {
+        return $this->packageConfig($key);
+    }
+
+    /**
+     * self::import  self::packageFetch  是一对操作  却别是给key加前缀
+     * @desc 导入配置 设置操作
+     * @param array $dependencies
+     */
+    public static function import(array $dependencies)
+    {
+        self::initContainer();
+        foreach ($dependencies as $key => $value) {
+            self::$container[static::prefix($key)] = $value;
+        }
+    }
+    /**
+     * @param $key
+     *
+     * @return mixed
+     */
     protected function packageFetch($key)
     {
         return self::$container[static::prefix($key)];
+    }
+
+
+    /**
+     * @desc 获取容器值 获取保存的信息 keyValue形式的获取
+     * @param $key
+     *
+     * @return mixed
+     */
+    protected function fetch($key)
+    {
+        return self::$container[$key];
     }
 
     /**
@@ -76,9 +107,12 @@ class Dependency
      * @param $key
      * @return string
      */
-    public static function prefix($key)
+    private static function prefix($key)
     {
         $class = static::class;
         return "$class.$key";
     }
+
+
+
 }
